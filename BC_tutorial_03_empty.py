2aa12b17990c7d74a058d0cd5bc74c4f31050d60
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2022-03-22 11:59:00 +0100

    video flips

--- BC_tutorial_03_empty.py
+++ BC_tutorial_03_empty.py
@@ -1,24 +1,47 @@
+import code
 import numpy as np
 import cv2
-
-# TODO capture webcam image
-
-
-# TODO get camera image parameters from get()
+import math
+import operator
 
 
-# TODO create a window for the video
+cap = cv2.VideoCapture(0)
+
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+codec = cap.get(cv2.CAP_PROP_CODEC_PIXEL_FORMAT)
 
 
-# TODO start a loop
+print("height : ", height)
+print("width: ", width)
+print("codec: ", codec)
 
-# TODO (in loop) read a camera frame and check if that was successful
+cv2.namedWindow("webcam picture", cv2.WINDOW_FREERATIO)
 
-# TODO (in loop) create four flipped tiles of the image
-
-# TODO (in loop) display the image
-
-# TODO (in loop) press q to close the window and exit the loop
-
+while(True):
+    # ret is a boolean that returns true if the frame is available.
+    # frame is an image array vector captured based on the default frames per second defined explicitly or implicitly
+    ret, frame = cap.read()
+  
+    if(ret):
+        img = np.zeros(frame.shape, np.uint8)
+        smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        img[:height//2, :width//2] = smaller_frame  # top left (original)
+        img[height//2:, :width//2] = cv2.flip(smaller_frame, 0)
+        img[:height//2, width//2:] = cv2.flip(smaller_frame, 1)
+        img[height//2:, width//2:] = cv2.flip(smaller_frame, -1)
+    # Display the resulting frame
+    cv2.imshow('frame', img)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+cap.release()
+# # Destroy all the windows
+cv2.destroyAllWindows()
 
 # TODO release the video capture object and window
